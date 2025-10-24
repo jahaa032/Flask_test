f5155f655906e21c50102748b40f15f5e82b4375
jahaa032 <jahaa032@osloskolen.no>
2025-10-24 14:28:40 +0200

    Delete button

--- starter/app.py
+++ starter/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, redirect, url_for
 
 app = Flask(__name__)
 DB_PATH = "blog.db"
@@ -23,16 +23,15 @@ def fetch_post_by_id(post_id: int):
         return None
     return [row["id"], row["title"], row["content"]]
 
-def slett_post():
-    con.execute("DELETE id FROM post")
-    con.execute("SELECT title FROM post WHERE id = ?")
-    boktitle = c.fetchone()
-    print(f"{boktitle} slettes")
-
+@app.route("/delete/<int:post_id>")
+def slett_post(post_id):
+    with sqlite3.connect(DB_PATH) as con:
+        con.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+    return redirect(url_for('index'))
 @app.route("/")
-def hello():
+def index():
     posts = fetch_all_posts()
-    return render_template("index.html", posts=posts)
+    return render_template('index.html', posts=posts)
 
 @app.route("/post/<int:post_id>")
 def post_detail(post_id):
@@ -44,8 +43,6 @@ def post_detail(post_id):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
-
-from flask import abort
 
 
 if __name__ == "__main__":
